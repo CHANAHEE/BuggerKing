841c60f46ee54398b81f6d6d93ef1ea6191c2bfe
CHANAHEE <113041686+CHANAHEE@users.noreply.github.com>
2023-11-11 21:12:29 +0900

    BuggerKing Program

---
+++ BuggerKingEvent_Macro/BuggerKingEvent_Macro/MacroMain.Designer.cs
@@ -0,0 +1,61 @@
+﻿namespace BuggerKingEvent_Macro
+{
+    partial class MacroMain
+    {
+        /// <summary>
+        /// 필수 디자이너 변수입니다.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 사용 중인 모든 리소스를 정리합니다.
+        /// </summary>
+        /// <param name="disposing">관리되는 리소스를 삭제해야 하면 true이고, 그렇지 않으면 false입니다.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 디자이너에서 생성한 코드
+
+        /// <summary>
+        /// 디자이너 지원에 필요한 메서드입니다. 
+        /// 이 메서드의 내용을 코드 편집기로 수정하지 마세요.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button_Start = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // button_Start
+            // 
+            this.button_Start.Location = new System.Drawing.Point(61, 29);
+            this.button_Start.Name = "button_Start";
+            this.button_Start.Size = new System.Drawing.Size(179, 70);
+            this.button_Start.TabIndex = 0;
+            this.button_Start.Text = "시 작";
+            this.button_Start.UseVisualStyleBackColor = true;
+            this.button_Start.Click += new System.EventHandler(this.button_Start_Click);
+            // 
+            // MacroMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(294, 137);
+            this.Controls.Add(this.button_Start);
+            this.Name = "MacroMain";
+            this.Text = "버거킹 매크로";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button_Start;
+    }
+}
+
---
+++ BuggerKingEvent_Macro/BuggerKingEvent_Macro/MacroMain.cs
@@ -0,0 +1,62 @@
+﻿using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using OpenQA.Selenium.Support.UI;
+using SeleniumExtras.WaitHelpers;
+using System.Threading;
+
+namespace BuggerKingEvent_Macro
+{
+    public partial class MacroMain : Form
+    {
+        public MacroMain()
+        {
+            InitializeComponent();
+        }
+
+        private void button_Start_Click(object sender, EventArgs e)
+        {
+            IWebDriver driver = new ChromeDriver();
+
+            driver.Navigate().GoToUrl("https://burgerkingevent.com/");
+
+            IWebElement button = driver.FindElement(By.XPath("//*[@id=\"main\"]/section[2]/div/img[2]"));
+
+            button.Click();
+
+            WebDriverWait wait = new WebDriverWait(driver, TimeSpan.FromSeconds(10));
+            Thread.Sleep(5000);
+
+            IWebElement elementWithDataWord_블 = driver.FindElement(By.CssSelector("[data-word='블']"));
+            IWebElement elementWithDataWord_양 = driver.FindElement(By.CssSelector("[data-word='양']"));
+            IWebElement elementWithDataWord_화 = driver.FindElement(By.CssSelector("[data-word='화']"));
+            IWebElement elementWithDataWord_와 = driver.FindElement(By.CssSelector("[data-word='와']"));
+            IWebElement elementWithDataWord_피 = driver.FindElement(By.CssSelector("[data-word='피']"));
+
+            elementWithDataWord_블.Click();
+            elementWithDataWord_양.Click();
+            elementWithDataWord_양.Click();
+            elementWithDataWord_블.Click();
+            elementWithDataWord_피.Click();
+            elementWithDataWord_화.Click();
+            elementWithDataWord_와.Click();
+            elementWithDataWord_와.Click();
+            elementWithDataWord_화.Click();
+            elementWithDataWord_와.Click();
+            elementWithDataWord_와.Click();
+            elementWithDataWord_화.Click();
+            elementWithDataWord_블.Click();
+
+        }
+
+
+    }
+}
